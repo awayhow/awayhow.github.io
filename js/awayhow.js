6e7f53b45da9a5d9b025a4694d201b0bb8a5f637
Josep Pages <76741266+awayhow@users.noreply.github.com>
2021-01-09 21:30:14 +0100

    react6

--- js/awayhow.js
+++ js/awayhow.js
@@ -1,6 +1,8 @@
 'use strict';
 
-class LoginControl extends React.Component {
+const e = React.createElement;
+
+class Hello extends React.Component {
     constructor(props) {
         super(props);
         this.handleLoginClick = this.handleLoginClick.bind(this);
@@ -14,66 +16,15 @@ class LoginControl extends React.Component {
         this.setState({isLoggedIn: false});
     }
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-        if (isLoggedIn) {
-            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        if (this.state.isLoggedIn) {
+            return e('button', { class: "awhColorDark", onClick: this.handleLogoutClick }, 'Logout');
         } else {
-            button = <LoginButton onClick={this.handleLoginClick} />;
+            return e('button', { class: "awhColorDark", onClick: this.handleLoginClick }, 'Login');
         }
-        return (
-            <div>
-                <Greeting isLoggedIn={isLoggedIn} />
-                {button}
-            </div>
-        );
     }
-}
-
-function Greeting(props) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting />;
-    }
-    return <GuestGreeting />;
-}
-  
-function UserGreeting(props) {
-    return <h1>Welcome back!</h1>;
-}
-  
-function GuestGreeting(props) {
-    return <h1>Please sign up.</h1>;
-}
-
-function LoginButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Login
-        </button>
-    );
-}
-  
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Logout
-        </button>
-    );
 }
 
 ReactDOM.render(
-    <LoginControl />,
-    document.getElementById('LoginControl')
+    e(Hello, null), 
+    document.getElementById('loginHandler')
 );
-
-// // Find all DOM containers, and render Like buttons into them.
-// document.querySelectorAll('.myContainer')
-//     .forEach(domContainer => {
-//         // Read the comment ID from a data-* attribute.
-//         const commentID = parseInt(domContainer.dataset.commentid, 10);
-//         ReactDOM.render(
-//             e(myContainer, { commentID: commentID }),
-//             domContainer
-//         );
-//     });
